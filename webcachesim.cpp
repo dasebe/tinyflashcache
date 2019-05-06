e1ebf7c31669f00c015add388a3cc1b2ec0ecf5e
Daniel S Berger <dsberger@cmu.edu>
2019-05-06 12:04:28 -0400

    restructured zipf into separate class

--- webcachesim.cpp
+++ webcachesim.cpp
@@ -1,166 +1,78 @@
 #include <fstream>
 #include <string>
-#include <regex>
 #include "caches/lru_variants.h"
 #include "caches/gd_variants.h"
 #include "request.h"
-#include <iostream>
-#include <functional>
-#include <string>
-#include <vector>
-#include <math.h>
+#include "zipf.h"
 
-using namespace std;
-
-typedef std::pair<int64_t, int64_t> IdSizePair;
-typedef std::pair<long double, std::vector<IdSizePair>*> CDFPair;
-
-std::mt19937_64 rd_gen;
-
-bool CDFCompare(const CDFPair& a, const CDFPair& b)
-{
-    return a.first < b.first;
-}
-
+//using namespace std;
 
 int main (int argc, char* argv[])
 {
     // output help if insufficient params
-    if(argc < 5) {
-        cerr << "webcachesim CacheType ObjCount TraceLength BucketCount BucketSize [cacheParams]" << endl;
+    if(argc != 6) {
+        std::cerr << "webcachesim CacheType ObjCount TraceLength BucketCount BucketSize\n";
         return 1;
     }
 
     // trace properties
-    const string cacheType = argv[1];
-    const int64_t ObjCount = std::stoull(argv[2]);
-    const int64_t TraceLength = std::stoull(argv[3]);
+    const std::string cacheType = argv[1];
+    const int64_t objCount = std::stoull(argv[2]);
+    const int64_t traceLength = std::stoull(argv[3]);
   
     // configure cache size
-    const uint64_t bucket_count  = std::stoull(argv[4]);
-    const uint64_t bucket_size  = std::stoull(argv[5]);
+    const uint64_t bucketCount  = std::stoull(argv[4]);
+    const uint64_t bucketSize  = std::stoull(argv[5]);
+
     // create buckets and their caches
-    std::vector<unique_ptr<Cache> > caches;
-    for(uint64_t i=0; i<bucket_count; i++) {
+    std::vector<std::unique_ptr<Cache> > caches;
+    for(uint64_t i=0; i<bucketCount; i++) {
         caches.push_back(move(Cache::create_unique(cacheType)));
-    }
-    for(uint64_t i=0; i<bucket_count; i++) {  
-        caches[i]->setSize(bucket_size);
+        caches[i]->setSize(bucketSize);
     }
 
-    // parse cache parameters
-    regex opexp ("(.*)=(.*)");
-    cmatch opmatch;
-    string paramSummary;
-    for(int i=6; i<argc; i++) {
-        regex_match (argv[i],opmatch,opexp);
-        if(opmatch.size()!=3) {
-            cerr << "each cacheParam needs to be in form name=value" << endl;
-            return 1;
-        }
-        for(uint64_t i=0; i<bucket_count; i++) {  
-            caches[i]->setPar(opmatch[1], opmatch[2]);
-        }
-        paramSummary += opmatch[2];
-    }
+    // init zipf distribution
+    auto zr = ZipfRequests("size.dist",objCount);
     
-
-    // init
-    rd_gen.seed(1);
-    // scan files from file
-    ifstream infile;
-    infile.open("size.dist");
-    int64_t tmp;
-    vector<int64_t> base_sizes;
-    while (infile >> tmp) {
-        if(tmp>0 && tmp<4096) {
-            base_sizes.push_back(tmp);
-        }
-    }
-    infile.close();
-    if(base_sizes.size()==0) {
-        base_sizes = vector<int64_t>({16,32,64,100,128,200,256,300,100,128,200,256,300,100,128,200,256,300,100,128,200,256,300,100,128,200,256,300,400,700,900,512,1024,800,1400,1500,2048,4096});
-    }
-    std::uniform_int_distribution<uint64_t> size_dist(0,base_sizes.size()-1);
-    std::vector<CDFPair> _popularityCdf; // CDF of popularity distribution, scaled by totalCount (i.e., range [0,totalCount])
-    std::unordered_map<long double, std::vector<IdSizePair>*> data;
-    int64_t sampled_size;
-    long double rate = 1e10L;
-    uint64_t div_factor = 1;
-    cerr << "done init\n";
-    // create high-level distribution datastructures
-    for(int64_t i=0; i<ObjCount;) {
-        rate /= 1.5;
-        std::vector<IdSizePair>*& idSizes = data[roundl(rate)+1.0L];
-        if (idSizes == NULL) {
-            idSizes = new std::vector<IdSizePair>();
-        }
-        // insert multiple ones at this rate
-        for(uint64_t j=0; j<div_factor; j++) {
-            sampled_size=base_sizes[size_dist(rd_gen)];
-            idSizes->emplace_back(i, sampled_size);
-            i++;
-        }
-        //cerr << i << " " << round(rate)+1 << " " << sampled_size << "\n";
-        div_factor *= 2;
-    }
-    cerr << "done dists\n";
-    // calculate popularity CDF
-    long double totalCount = 0;
-    for (auto &it : data) {
-        long double count = it.first;
-        std::vector<IdSizePair>* idSizes = it.second;
-        totalCount += count * idSizes->size();
-        //cerr << "calc " << count << " " << (*idSizes)[0].first << " " << totalCount << "\n";
-        _popularityCdf.emplace_back(totalCount, idSizes);
-    }
-    std::uniform_real_distribution<long double> _distribution(0, totalCount - 1);
-    cerr << "done cdf\n";
-
     // running the simulator
     uint64_t reqs = 0, hits = 0;
     size_t hh, bucket_idx;
     std::hash<std::string> hash;
     SimpleRequest* req = new SimpleRequest(0, 0);
-    cerr << "running..." << endl;
-    // balls and bins counters
-    std::vector<int64_t > ballsbins(bucket_count);
-    std::vector<int64_t > ballsbins_misses(bucket_count);
 
-    for(int64_t i=0; i<TraceLength; i++) {
-        // sample
-        long double r = _distribution(rd_gen);
-        const std::vector<CDFPair>::iterator it = std::upper_bound(_popularityCdf.begin(), _popularityCdf.end(), CDFPair(r, NULL), CDFCompare);
-        const std::vector<IdSizePair>& idSizes = *(it->second);
-        size_t r2 = std::uniform_int_distribution<size_t>(0, idSizes.size() - 1)(rd_gen);
-        const IdSizePair& idSize = idSizes[r2];
-        req->reinit(idSize.first,idSize.second); // id, size
-        // simulate
+    // balls and bins counters
+    std::vector<int64_t > ballsbins(bucketCount);
+    std::vector<int64_t > ballsbins_misses(bucketCount);
+
+    for(int64_t i=0; i<traceLength; i++) {
+        zr.Sample(req);
         reqs++;
-        hh=hash(std::to_string(idSize.first));
-        bucket_idx = hh % bucket_count;
-        // ballsandbins
-        ballsbins[bucket_idx]+=idSize.second;
+
+        // hash into bin
+        hh=hash(std::to_string(req->getId()));
+        bucket_idx = hh % bucketCount;
+        // ballsandbins stats
+        ballsbins[bucket_idx]+=req->getId();
         // caching
         if(caches[bucket_idx]->lookup(req)) {
             hits++;
         } else {
             caches[bucket_idx]->admit(req);
-            // ballsandbins (only cache misses)
-            ballsbins_misses[bucket_idx]+=idSize.second;
+            // ballsandbins stats (for only cache misses)
+            ballsbins_misses[bucket_idx]+=req->getSize();
         }
-        // output
+        // progress output
         if(reqs % 1000000 == 0) {
-            cerr << double(hits)/reqs << "\n";
+            std::cerr << double(hits)/reqs << "\n";
         }
     }
 
     delete req;
 
-    // stats gathering
+    // stats calculation and output
     int64_t maxballs=0, minballs, sumballs=0;
     int64_t maxballs_misses=0, minballs_misses, sumballs_misses=0;
-    for(uint64_t i=0; i<bucket_count; i++) {
+    for(uint64_t i=0; i<bucketCount; i++) {
         if(ballsbins[i]>maxballs) {
             maxballs=ballsbins[i];
         }
@@ -172,7 +84,7 @@ int main (int argc, char* argv[])
     }
     minballs=maxballs;
     minballs_misses=maxballs_misses;
-    for(uint64_t i=0; i<bucket_count; i++) {
+    for(uint64_t i=0; i<bucketCount; i++) {
         if(ballsbins[i]<minballs) {
             minballs=ballsbins[i];
         }
@@ -181,9 +93,9 @@ int main (int argc, char* argv[])
         }    
     }
 
-    cout << cacheType << " " << ObjCount << " "
-         << bucket_count << " " << bucket_size << " "
-         << paramSummary << " "
+    std::cout << cacheType << " " << objCount << " "
+         << bucketCount << " " << bucketSize << " "
+         << " " // old/unused output
          << double(hits)/reqs << " " 
          << maxballs << " "
          << minballs << " "
@@ -191,7 +103,7 @@ int main (int argc, char* argv[])
          << maxballs_misses << " "
          << minballs_misses << " "
          << double(sumballs_misses)/caches.size() << " "
-         << endl;
+         << "\n";
     
     return 0;
 }
---
+++ zipf.h
@@ -0,0 +1,87 @@
+#include <math.h>
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+
+class ZipfRequests {
+
+private:
+    typedef std::pair<int64_t, int64_t> IdSizePair;
+    typedef std::pair<long double, std::vector<IdSizePair>*> CDFPair;
+
+    std::mt19937_64 rd_gen;
+    std::uniform_real_distribution<long double> _distribution;
+    std::vector<CDFPair> _popularityCdf; // CDF of popularity distribution, scaled by totalCount (i.e., range [0,totalCount])
+
+    static bool CDFCompare(const CDFPair& a, const CDFPair& b)
+    {
+        return a.first < b.first;
+    }
+
+public:
+    ZipfRequests(std::string sizeFile, int64_t objCount) {
+        // init
+        rd_gen.seed(1);
+        // scan files from file
+        std::vector<int64_t> base_sizes;
+        if(sizeFile.size()>0) {
+            std::ifstream infile;
+            infile.open(sizeFile);
+            int64_t tmp;
+            while (infile >> tmp) {
+                if(tmp>0 && tmp<4096) {
+            base_sizes.push_back(tmp);
+                }
+            }
+            infile.close();
+        }
+        if(base_sizes.size()==0) {
+            base_sizes = std::vector<int64_t>({16,32,64,100,128,200,256,300,100,128,200,256,300,100,128,200,256,300,100,128,200,256,300,100,128,200,256,300,400,700,900,512,1024,800,1400,1500,2048,4096});
+        }
+        std::uniform_int_distribution<uint64_t> size_dist(0,base_sizes.size()-1);
+        int64_t sampled_size;
+        long double rate = 1e10L;
+        uint64_t div_factor = 1;
+        std::unordered_map<long double, std::vector<IdSizePair>*> data;
+
+        // create high-level distribution datastructures
+        for(int64_t i=0; i<objCount;) {
+            std::vector<IdSizePair>*& idSizes = data[roundl(rate)+1.0L];
+            if (idSizes == NULL) {
+                idSizes = new std::vector<IdSizePair>();
+            }
+            // insert multiple objects at this rate
+            for(uint64_t j=0; j<div_factor; j++) {
+                sampled_size=base_sizes[size_dist(rd_gen)];
+                idSizes->emplace_back(i, sampled_size);
+                i++;
+            }
+            rate /= 1.5;  // reduce rate
+            div_factor *= 2;  // increase obj count at next rate
+        }
+
+        // calculate popularity CDF
+        long double totalCount = 0;
+        for (auto &it : data) {
+            long double count = it.first;
+            std::vector<IdSizePair>* idSizes = it.second;
+            totalCount += count * idSizes->size();
+            _popularityCdf.emplace_back(totalCount, idSizes);
+        }
+        _distribution = std::uniform_real_distribution<long double>(0, totalCount - 1);
+    }
+
+
+    void Sample(SimpleRequest * req) {
+        // sample
+        long double r1 = _distribution(rd_gen);
+        const std::vector<CDFPair>::iterator it = std::upper_bound(_popularityCdf.begin(), _popularityCdf.end(), CDFPair(r1, NULL), CDFCompare);
+        const std::vector<IdSizePair>& idSizes = *(it->second);
+        size_t r2 = std::uniform_int_distribution<size_t>(0, idSizes.size() - 1)(rd_gen);
+        const IdSizePair& idSize = idSizes[r2];
+        req->reinit(idSize.first,idSize.second); // id, size
+    }
+};
